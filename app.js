601c8c45a976648da0d962e9cb07726ec23ed459
milanvt <milan.van.trimpont@student.ehb.be>
2025-01-17 17:41:46 +0100

    nieuwe kleren kunnen aangemaakt worden

--- app.js
+++ app.js
@@ -10,19 +10,43 @@ app.get('/', (req, res) =>
     res.send('test');
   });
 
-// alle gebruikers ophalen uit de db
-app.get('/users', async (req, res) => {
+// alle kleding stukken ophalen uit de db
+app.get('/kleding_items', async (req, res) => {
   try 
   {
-    const [rows] = await pool.query('SELECT * FROM users'); 
+    const [rows] = await pool.query('SELECT * FROM kleding_items'); 
     res.json(rows);
   } 
   catch (err) 
   {
-    console.error('Error fetching users:', err.message);
+    console.error('Error fetching kleding items:', err.message);
     res.status(500).send('Error retrieving data from the database');
   }
 });
+
+// nieuwe kleren toevoegen
+app.post('/kleding_items/toevoegen', async (req, res) => {
+    const { titel, content } = req.body; 
+    
+    try {
+      // Voer een INSERT query uit
+      const [result] = await pool.query(
+        'INSERT INTO kleding_items (titel, content) VALUES (?, ?)',
+        [titel, content] 
+      );
+      
+      // bericht of het gelukt is of niet
+      res.status(201).json({ 
+        message: 'Data succesvol toegevoegd', 
+        insertId: result.insertId 
+      });
+    } catch (err) {
+      console.error('Fout bij toevoegen van data:', err);
+      res.status(500).send('Er is een fout opgetreden bij het toevoegen van data');
+    }
+  });
+
+ 
 
 // Server starten
 const PORT = process.env.PORT || 3000;
